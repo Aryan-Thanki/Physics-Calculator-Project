53c9a8f174292c4e5926b22f90f29cfb888c5d69
Aryan Thanki <aryanthanki@rhs.school.nz>
2020-08-26 12:34:09 +1200

    Add project files.

---
+++ Physics Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Physics_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graphics paper = this.CreateGraphics();
+            SolidBrush brush = new SolidBrush(Color.CornflowerBlue);
+            Rectangle rect = new Rectangle(30, 350, 50, 50);
+            paper.FillRectangle(brush, rect);
+
+            int i = 0;
+
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
